fe89b7dc4099639bcbd4538ad843078b919be137
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-10-24 16:48:23 +0400

    Add project files.

---
+++ api-ver2/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+﻿using api_ver2.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_ver2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly List<Student>? _students;
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_students);
+        }
+
+        [HttpGet("{id?}")]
+        public IActionResult GetAll(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var student = _students?.Find(x => x.Id == id);
+
+            if (student == null) return NotFound();
+
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            return Created("Test", student);
+        }
+
+        [HttpPost("primitive-parameters")]
+        public IActionResult PrimitiveTypes([FromBody] int some)
+        {
+            return Created("Test", some);
+        }
+    }
+}
+    
---
+++ api-ver2/Controllers/StudentsV2Controller.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_ver2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("2.0")]
+    public class StudentsV2Controller : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        [HttpPost("Upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var path = Path.Combine(_webHostEnvironment.WebRootPath, "images", file.FileName);
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            path = Path.Combine(path, file.FileName);
+
+            using (var fs = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(fs);
+            }
+
+            return Ok(file);
+        }
+
+        [HttpGet("Download/{fileName}")]
+        public IActionResult Download(string fileName)
+        {
+            if (fileName == null) return NotFound();
+
+            var path = Path.Combine(_webHostEnvironment.WebRootPath, "images", fileName);
+
+            var bytes = System.IO.File.ReadAllBytes(path);
+
+            return File(bytes, "Test/txt", fileName);
+        }
+    }
+}
